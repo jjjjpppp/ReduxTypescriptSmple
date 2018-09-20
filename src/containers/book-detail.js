d9964b088b65a1c46b7046fd561b5fa0be86230e
Jun Ishioka <jjjjpppp25@gmail.com>
2018-09-20 16:49:12 +0900

    booklist

---
+++ src/containers/book-detail.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { selectBook } from '../actions/index'
+import { bindActionCreators } from 'redux';
+
+export class BookDetail extends Component {
+  render() {
+    if (!this.props.book) {
+      return <div>Select a book to get started.</div>
+    }
+
+    return(
+      <div>
+        <h3>Title: {this.props.book.title}</h3>
+        <h3>Pages: {this.props.book.pages}</h3>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    book: state.activeBook
+  }
+}
+
+export default connect(mapStateToProps)(BookDetail);
